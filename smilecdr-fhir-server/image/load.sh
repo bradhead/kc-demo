dd04ad3c5f823ebf9375297d4a2fafa0563ab29c
bradhead <brad.head@gmail.com>
2023-07-12 12:11:18 -0700

    update smile cdr

--- smilecdr-fhir-server/image/load.sh
+++ smilecdr-fhir-server/image/load.sh
@@ -1,4 +1,4 @@
 #!/bin/sh 
 # load docker image and tag it.
-docker image load -i smilecdr-2022.11.R03-docker.tar
-docker tag smilecdr:latest smilecdr:2022.11.R03
+docker image load -i smilecdr-2023.05.R01-docker.tar
+docker tag smilecdr:latest  smilecdr:2023.05.R01
