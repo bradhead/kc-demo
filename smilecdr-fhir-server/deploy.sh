51f0772c90cc062a713aeb4f254497edc74c27ab
bradhead <brad.head@gmail.com>
2023-08-23 18:27:42 -0700

    updates

--- smilecdr-fhir-server/deploy.sh
+++ smilecdr-fhir-server/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 # start the image
 
+echo 'stopping smilecdr...'
+docker stop smilecdr-2023.05.R02
+docker rm smilecdr-2022.05.R02
+echo 'loading the image for smilecdr...'
+docker image load -i image/smilecdr-2023.05.R02-docker.tar
+docker tag smilecdr:latest  smilecdr:2023.05.R02
 docker compose up -d --build
--- smilecdr-fhir-server/shell.sh
+++ smilecdr-fhir-server/shell.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-# open a sheel in the docker container
+# open a shell in the docker container
 docker exec -it smilecdr-2022.11.R03 /bin/bash
